6c2d70fdbdac834d11309388ad30f6d472799fe0
nicoleaqno <nicole.aquino.rosa@escola.pr.gov.br>
2025-07-04 12:54:51 +0000

    ok

---
+++ Perguntas.js
@@ -0,0 +1,45 @@
+Perguntas.js
+criaCartao(
+'Português',
+'O que é susbstantivo?',
+'Palavra que nomeia seres, coisas, lugares, etc.'
+)
+
+criaCartao(
+'Geografia',
+'Quais são os continentes do mundo?',
+'América, Europa, Ásia, África, Oceania e Antártica'
+)
+
+criaCartao(
+'História',
+'Quem descobriu o Brasil?',
+'Pedro Álvares Cabral'
+)
+criaCartao(
+'Ciências',
+'Quais são os estados físicos da água?',
+'Sólido, líquido e gasoso'
+)
+criaCartao(
+'Matemática',
+'Qual a fórmula da área do quadrado?',
+'Lado a lado'
+)
+
+criaCartao(
+'Geografia',
+'Qual a capital da França?',
+'A capital da França é Paris'
+)
+
+criaCartao(
+'Programação',
+'O que é uma função?',
+'Uma função é um bloco de código que executa alguma tarefa'
+)
+criaCartao(
+'Lingua inglesa',
+'Como se diz OI em inglês?',
+'Oi em ingles é HI (RAI)'
+)
